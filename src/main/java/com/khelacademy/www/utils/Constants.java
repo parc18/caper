bcb96605abbe7bcc9d692e9ff0b116031b60fa59
Manish Premi <manishpremi20@gmail.com>
2018-09-28 17:14:23 +0530

    webhook

--- src/main/java/com/khelacademy/www/utils/Constants.java
+++ src/main/java/com/khelacademy/www/utils/Constants.java
@@ -1,11 +1,18 @@
 package com.khelacademy.www.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Constants {
     public static final int SUCCESS_RESPONSE_CODE = 200;
     public static final int BAD_REQUEST_CODE = 400;
     public static final String SUCCESS_RESPONSE_VALUE = "SUCCESS!!";
     public static final String FAILURE_RESPONSE_VALUE = "FAILURE!!";
     public static final String REDIRECT_URL = "http://www.khelacademy.com/thankyou";
-    public static final String WEBHOOK_URL = "https://capers.herokuapp.com/api/instamojo_webhook/";
+    public static String WEBHOOK_URL;
+    
+    @Value("${dbPass}")
+    private void setDbPass(String dbPass) {
+    	Constants.WEBHOOK_URL = dbPass;
+	}
     
 }
