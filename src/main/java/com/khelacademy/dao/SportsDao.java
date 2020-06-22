221b532ad0f582d4d2090f1fac720ef638431927
Manish Premi <manish@wizely.in>
2020-06-22 20:39:26 +0530

    try

--- src/main/java/com/khelacademy/dao/SportsDao.java
+++ src/main/java/com/khelacademy/dao/SportsDao.java
@@ -2,6 +2,11 @@ package com.khelacademy.dao;
 
 import javax.ws.rs.core.Response;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component
 public interface SportsDao {
 	Response getAllSports();
 }
